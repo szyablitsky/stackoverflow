774a580c60afa05135f246384ed36d8e30f1ff08
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2014-05-14 03:07:52 -0700

    database cleaner setup for js testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,6 @@ RSpec.configure do |config|
   config.include FeatureMacros, type: :feature
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
     begin
       DatabaseCleaner.start
@@ -40,6 +39,14 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
     DatabaseCleaner.start
   end
 
@@ -47,25 +54,7 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.use_transactional_fixtures = false
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = 'random'
 end

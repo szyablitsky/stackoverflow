d16b26f5a8ee3d5aeb16848f7c5bf83d65ca0c64
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2014-05-14 03:56:53 -0700

    spec refactoring (follow up)

---
+++ spec/features/features_helper.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+RSpec.configure do |config|
+  config.include FeatureMacros, type: :feature
+
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
